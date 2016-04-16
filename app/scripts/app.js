b501942c3f1fc12e18002c4e503a9178e8a2751b
Mauro <mgparada@esei.uvigo.es>
2016-04-16 17:24:53 +0200

    Adding new routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,23 +13,72 @@ angular
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('index', {
+        url: '/',
+        views: {
+          'header': {
+            templateUrl: 'views/index/header.html'
+          },
+          'body': {
+            templateUrl: 'views/index/body.html'
+          },
+          'footer': {
+            templateUrl: 'views/index/footer.html'
+          }
+        }
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+      .state('user', {
+        url: '/user',
+        abstract: true,
+        views: {
+          'header': {
+            template: '<div ui-view="header"></div>'
+          },
+          'body': {
+            template: '<div ui-view="body"></div>'
+          },
+          'footer': {
+            template: '<div ui-view="footer"></div>'
+          }
+        }
       })
-      .otherwise({
-        redirectTo: '/'
+      .state('user.signup', {
+        url: '/signup',
+        views: {
+          'header': {
+            templateUrl: 'views/user/signup/header.html'
+          },
+          'body': {
+            templateUrl: 'views/user/signup/body.html'
+          },
+          'footer': {
+            templateUrl: 'views/user/signup/footer.html'
+          }
+        }
+      })
+      .state('user.login', {
+        url: '/login',
+        views: {
+          'header': {
+            templateUrl: 'views/user/signin/header.html'
+          },
+          'body': {
+            templateUrl: 'views/user/signin/body.html'
+          },
+          'footer': {
+            templateUrl: 'views/user/signin/footer.html'
+          }
+        }
       });
+
+    $locationProvider.html5Mode(true);
   });
